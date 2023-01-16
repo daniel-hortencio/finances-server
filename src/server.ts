e40a04182d3a54b37b3b4dafc7f2cab5f1d9c8ef
Daniel Hortencio <hortencio.daniel@gmail.com>
2023-01-16 18:34:28 -0300

    config serverless

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata"
 import express from 'express'
 import cors from 'cors'
+import serverless from 'serverless-http'
 import "express-async-errors"
 import { router } from './shared/infra/routes'
 import swaggerUi from 'swagger-ui-express'
@@ -20,7 +21,7 @@ server.use(cors({
 
 server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
-server.use(router)
+server.use("./netlify/functions/server", router)
 
 server.use(ErrorHandler)
 
