532c241e8ac95fee9894be5d7f0569b6bde56314
Daniel Hortencio <hortencio.daniel@gmail.com>
2023-01-18 12:36:45 -0300

    updates package scripts

--- build/shared/enums/languages.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Language = void 0;
-var Language;
-(function (Language) {
-    Language["PT"] = "PT";
-    Language["ESP"] = "ESP";
-    Language["ENG"] = "ENG"; // English
-})(Language = exports.Language || (exports.Language = {}));
